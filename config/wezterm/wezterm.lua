5402d5ab96b2d0b30a5671da27bbb0a5610943fe
Ben R <breeves715@pm.me>
2025-01-03 13:17:52 -0500

    add ghostty config, unify wez config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -9,9 +9,11 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 config.color_scheme = 'Catppuccin Mocha'
 
+-- font stuff
 config.font = wezterm.font("Hack Nerd Font Mono")
 config.font_size = 13
 
+-- tab bar and window styling
 config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
@@ -21,6 +23,9 @@ config.inactive_pane_hsb = {
     saturation = 0.8,
     brightness = 0.7,
 }
+
+-- wayland support isn't there quite yet so we turn that off
+config.enable_wayland = false
 
 -- and finally, return the configuration to wezterm
 return config
